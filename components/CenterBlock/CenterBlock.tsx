db65e33db5aa800a2d20b9646962a9cc83d32e7e
Olga <olga.machulnaya@gmail.com>
2026-07-24 15:18:40 +0300

    Правка 3

--- components/CenterBlock/CenterBlock.tsx
+++ components/CenterBlock/CenterBlock.tsx
@@ -1,5 +1,5 @@
 import SearchBar from "./SearchBar";
-import Filter from "./Filter";
+import Filter from "./Filter/Filter";
 import Playlist from "./Playlist/Playlist";
 import styles from "./CenterBlock.module.css";
 
--- components/CenterBlock/Playlist/TrackItem.tsx
+++ components/CenterBlock/Playlist/TrackItem.tsx
@@ -1,21 +1,19 @@
 import Link from "next/link";
+import type { Track } from "@/data";
 import styles from "./TrackItem.module.css";
 
 interface TrackItemProps {
-  title: string;
-  author: string;
-  album: string;
-  duration: string;
-  titleSpan?: string;
+  track: Track;
+  }
+
+function formatDuration(durationInSeconds: number): string {
+  const minutes = Math.floor(durationInSeconds / 60);
+  const seconds = durationInSeconds % 60;
+
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
 }
 
-export default function TrackItem({
-  title,
-  author,
-  album,
-  duration,
-  titleSpan,
-}: TrackItemProps) {
+export default function TrackItem({ track }: TrackItemProps) {
   return (
     <div className={styles.playlist__item}>
       <div className={styles.playlist__track}>
@@ -27,28 +25,27 @@ export default function TrackItem({
           </div>
           <div className={styles.track__titleText}>
             <Link href="#" className={styles.track__titleLink}>
-              {title}{" "}
-              {titleSpan && (
-                <span className={styles.track__titleSpan}>{titleSpan}</span>
-              )}
+              {track.name}
             </Link>
           </div>
         </div>
         <div className={styles.track__author}>
           <Link href="#" className={styles.track__authorLink}>
-            {author}
+            {track.author}
           </Link>
         </div>
         <div className={styles.track__album}>
           <Link href="#" className={styles.track__albumLink}>
-            {album}
+            {track.album}
           </Link>
         </div>
         <div className={styles.track__time}>
           <svg className={styles.track__timeSvg}>
             <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
           </svg>
-          <span className={styles.track__timeText}>{duration}</span>
+          <span className={styles.track__timeText}>
+            {formatDuration(track.duration_in_seconds)}
+          </span>
         </div>
       </div>
     </div>
